de720d89007397f5040b7eb23abdc9482387f337
Kyle-Neale <kyle.a.neale@gmail.com>
2018-12-10 17:35:18 -0500

    Added BCrypt to package.json file to use with User authentication.

--- slack-clone-backend/src/graphql/resolvers/user.js
+++ slack-clone-backend/src/graphql/resolvers/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export default {
 
   Query: {
@@ -5,6 +7,15 @@ export default {
     allUsers: async (parent, args, { models }, info) => await models.User.findAll()
   },
   Mutation: {
-    createUser: async (parent, args, { models }, info) => await models.User.create(args)
+    registerUser: async (parent, {password, ...otherArgs}, { models }, info) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await models.User.create({ ...otherArgs , password: hashedPassword });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    }
   }
 }
