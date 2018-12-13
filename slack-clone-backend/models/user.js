dfcf54ea146650a1e99a03559626d220e687690a
Kyle-Neale <kyle.a.neale@gmail.com>
2018-12-13 17:29:31 -0500

    Added some validations to the Users model

--- slack-clone-backend/models/user.js
+++ slack-clone-backend/models/user.js
@@ -2,13 +2,33 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contains letters and numbers.'
+        },
+        len: {
+          args: [3, 25],
+          msg: 'The username needs to be between 3 and 25 characters long.'
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
       unique: true
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email'
+        }
+      }
     },
-    password: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING},
+      validates: {
+        len: [6, 16]
+      }
   });
 
   User.associate = (models) => {
