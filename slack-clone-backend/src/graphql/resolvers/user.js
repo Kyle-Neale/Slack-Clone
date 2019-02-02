d621e274bd5164255601eb5a491aac127e57ef7e
Kyle-Neale <kyle.a.neale@gmail.com>
2019-02-02 08:48:31 -0500

    Doing some refactoring of backend specifically with BCrypt.

--- slack-clone-backend/src/graphql/resolvers/user.js
+++ slack-clone-backend/src/graphql/resolvers/user.js
@@ -22,34 +22,19 @@ export default {
   },
   Mutation: {
     loginUser: (parent, { email, password }, { models, SECRET, SECRET2 }) => tryLogin(email, password, models, SECRET, SECRET2),
-    registerUser: async (parent, { password, ...otherArgs }, { models }, info) => {
+    registerUser: async (parent, args, { models }, info) => {
       try {
-        if (password.length < 5 || password.length > 25) {
-          return {
-            ok: false,
-            errors: [
-              {
-                path: 'password',
-                message: 'The password needs to be between 5 and 25 characters long.'
-              }
-            ]
-          }
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        const user = await models.User.create(args);
         return {
           ok: true,
           user,
         };
       } catch (err) {
-        console.log(err);
         return {
           ok: false,
           errors: formatErrors(err, models),
         };
       }
     }
-
   }
 }
