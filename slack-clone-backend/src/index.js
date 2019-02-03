286ae3e2b153655683ae2c20850307b7d14ed3b5
Kyle-Neale <kyle.a.neale@gmail.com>
2019-02-03 10:31:00 -0500

    Integrated the use of express in backend

--- slack-clone-backend/src/index.js
+++ slack-clone-backend/src/index.js
@@ -1,36 +1,37 @@
-import { ApolloServer, gql } from 'apollo-server';
+import express from 'express';
+import bodyParser from 'body-parser';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 import jwt from 'jsonwebtoken';
-import models from '../models';
+import cors from 'cors';
 
-const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './graphql/schema')));
-const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './graphql/resolvers')));
+import models from '../models';
+import { refreshTokens } from './auth';
 
 const SECRET = 'asdnain12o31odsalno2n35'
 const SECRET2 = 'asdnain12o3o2n351235123112asda213eqdsyherd5ysv'
 
-// const getUser = (req) => {
-//   const token = req.headers['x-token'];
-//   if (token) {
-//     try {
-//       const { user } = jwt.verify(token, secret);
-//       req.user = user;
-//     } catch (error) {
-//       const refreshToken = req.headers['x-refresh-token'];
-//       const newTokens = await refreshTokens(token, refreshToken, models, SECRET);
-//       if (newTokens.token && newToken.refreshToken) {
-//
-//       }
-//     }
-//   }
-// }
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './graphql/schema')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './graphql/resolvers')));
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+app.use(cors('*'));
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: {
+    endpoint: `http:localhost:3000/graphql`
+  },
   context: ({ req }) => {
-    console.log(req.headers)
     return {
       user: {
         id: 1
@@ -42,11 +43,31 @@ const server = new ApolloServer({
   }
 });
 
+server.applyMiddleware({ app });
+
+const getUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, secret);
+      req.user = user;
+    } catch (error) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET);
+      if (newTokens.token && newToken.refreshToken) {
+      }
+    }
+  }
+  next();
+};
+
+app.use(getUser);
+
+
+const PORT = 4000;
 
 models.sequelize.sync({force: true}).then(() => {
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  app.listen(PORT, () => {
+    console.log(`The server has started on port: ${PORT}`);
   });
 })
-
-// export default mergeTypes(typeDefs, { all: true });
